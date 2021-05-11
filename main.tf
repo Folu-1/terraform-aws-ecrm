851c1ce7ee2971dbb5a75cfbd8279c6f6827fb3f
Folu-1 <Fiyintokode@hotmail.com>
2021-05-11 13:18:02 -0400

    hazelops ecr model

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
-resource "aws_ecr_repository" "foo" {
+module "ecr" {
+  
+  source = "hazelops/ecr/aws"
+  version = "~> 1.0"
   name = var.name
-  image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
+
+
+
+// terraform {
+//  required_providers {
+//    aws = {
+//      source  = "hashicorp/aws"
+//      version = "3.39.0"
+//    }
+//  }
+// }
+
+# Configure the AWS Provider
+// provider "aws" {
+//  region = "us-east-1"
+// }
+
+// resource "aws_ecr_repository" "foo" {
+//  name = var.name
+//  image_tag_mutability = "MUTABLE"
+
+###
+# image_scanning_configuration {
+#    scan_on_push = true
+//  }
+//}
+
